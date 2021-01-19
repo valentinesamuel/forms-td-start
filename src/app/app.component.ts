8c176256e4c5b61707f0cd0258f96f392fd1f0c9
valentinesamuel <valentinesamuel2580@gmail.com>
2021-01-19 12:20:29 +0100

    (FEAT):Leant how to use @viewchild to get the data instead of passing the local refrence to the method in the HTML file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+  @ViewChild('f') signupForm: NgForm;
+  // suggestUserName() {
+  //   const suggestedName = 'Superuser';
+  // }
 
-onSubmit(form:NgForm){
-  console.log(form);
+// onSubmit(form:NgForm){
+//   console.log(form);
+// }
 
+onSubmit(){
+console.log(this.signupForm);
 }
 
 }
